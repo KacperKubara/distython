93b7688b84bcaf85dc3770dad5daf55e0430de4b
Kacper Kubara <kjk1u17@soton.ac.uk>
2019-07-14 15:31:34 +0100

    tests are complete and passing

--- HEOM_test.py
+++ HEOM_test.py
@@ -1,12 +1,21 @@
 import pytest
+import numpy as np 
 from HEOM import HEOM
+
 @pytest.fixture
 def params():
-    return {'data': 0, 'col_ix': 0, 'nan_eqvs': 0, 'result': 0}
+    return {'data': np.asarray([[1, np.nan, 5], [2, 2, 2], [1, 5, 3]]),
+             'col_ix': [0], 'nan_eqvs': [np.nan], 'result': [3.0, 1.44, 2.11]}
 
-def init_test(params):
+def test_init(params):
     metric = HEOM(params['data'], params['col_ix'], params['nan_eqvs'])
+    assert (metric.col_max == [2, 5, 5]).all()
     return metric
 
-def heom_test(init_test, params):
-    metric = init_test(params)
+def test_heom(params):
+    metric = test_init(params)
+    data = params['data']
+    results = params['result']
+    assert results[0] == round(metric.heom(data[0], data[1]), 2)
+    assert results[1] == round(metric.heom(data[0], data[2]), 2)
+    assert results[2] == round(metric.heom(data[1], data[2]), 2)

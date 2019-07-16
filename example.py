40bbdba360283260764db9807670560235a618f5
Kacper Kubara <kjk1u17@soton.ac.uk>
2019-07-16 21:53:35 +0100

    Example file completed

--- example.py
+++ example.py
@@ -7,17 +7,28 @@ from HEOM import HEOM
 
 # Load the dataset from sklearn
 boston = load_boston()
-boston_data= boston["data"]
+boston_data = boston["data"]
 # Categorical variables in the data
 categorical_ix = [3, 8]
-
+# Set up the NaN equivalent
+nan_eqv = 12345
 # Introduce some missingness to the data
 row_cnt, col_cnt = boston_data.shape
 for i in range(row_cnt):
     for j in range(col_cnt):
         rand_val = np.random.randint(20, size=1)
         if rand_val == 10:
-            boston_data[i, j] = np.nan
+            boston_data[i, j] = nan_eqv
 
-# Declare NearestNeighbor
-neighbor = NearestNeighbors()
+# Declare the HEOM
+heom_metric = HEOM(boston_data, categorical_ix, nan_equvialents = [nan_eqv])
+
+# Declare NearestNeighbor and set metric as heom
+neighbor = NearestNeighbors(metric = heom_metric.heom)
+
+# Fit the model
+neighbor.fit(boston_data)
+
+# Return 5-Nearest Neighbors to the 1st instance (row 1)
+result = neighbor.kneighbors(boston_data[0].reshape(1, -1), n_neighbors = 5)
+print(result)

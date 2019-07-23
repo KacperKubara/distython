121821a3f806b68272d3a677ac5831edbd7edb79
Kacper Kubara <kjk1u17@soton.ac.uk>
2019-07-23 20:38:42 +0100

    Bug fixes

--- VDM.py
+++ VDM.py
@@ -11,7 +11,7 @@ class VDM():
         print("X[:, self.cat_ix]: {}".format(X[:, self.cat_ix]))
         
         array_len = 0
-        # Get the max no. of unique classes for columns to initialize the array
+        # Get the max no. of unique classes within columns to initialize the array
         for ix in self.cat_ix:
             print("np.unique(X[:, ix]): {}".format(np.unique(X[:, ix])))
             max_val = len(np.unique(X[:, ix]))
@@ -19,14 +19,14 @@ class VDM():
                 array_len = max_val
                 print("max_val: {}".format(max_val))
         # Store the list of unique classes elements for each categorical column
+        # self.col_ix is used here for clearer indices assignment
         self.unique_attributes = np.full((array_len, len(self.col_ix)), fill_value=-1)
         print(self.unique_attributes)
         for ix in self.cat_ix:
             unique_vals = np.unique(X[:, ix])
             self.unique_attributes[0:len(unique_vals), ix] = unique_vals
-            print("unique vals: {}\n self.unique_attributes[:, ix]: {} \n".format(unique_vals, self.unique_attributes[:, ix]))
+            print("unique vals: {}\n self.unique_attributes: {} \n".format(unique_vals, self.unique_attributes))
         
-        self.unique_attributes_cnt = np.zeros(len(self.col_ix))
         # Declare the 3D numpy array which holds specifc count for each attribute
         # for each column for each output class
         # +1 in len(self.classes) + 1 is to store the sum (N_a,x) in the last element
@@ -36,15 +36,14 @@ class VDM():
         for i, col in enumerate(self.cat_ix):
             # For each attribute value in the column
             for j, attr in enumerate(self.unique_attributes[:, col]):
+                # If attribute exists
                 if attr != -1:
                     # For each output class value
                     for k, val in enumerate(self.classes):
                         # Get an attribute count for each output class
-                        row_ix = np.argwhere(X[:, col] == attr)
-                        print("np.sum(X[row_ix, y_ix] == val): {}".format(np.sum(X[row_ix, y_ix] == val)))
-                        cnt = np.sum(X[row_ix, y_ix] == val)
+                        row_ixs = np.argwhere(X[:, col] == attr)
+                        cnt = np.sum(X[row_ixs, y_ix] == val)
                         self.final_count[i, j, k] = cnt
-                        print(self.final_count)
                     # Get a sum of all occurences
                     self.final_count[i, j, -1] = np.sum(self.final_count[i, j, :])
         print("cat_ix: {}".format(cat_ix))
@@ -77,20 +76,28 @@ class VDM():
         """
         result = np.zeros(len(x))
 
-        for i in range(len(self.cat_ix)):
+        for i in self.cat_ix:
             # Get indices to access the final_count array 
-            x_ix = np.argwhere(self.final_count[i] == x[i])
-            y_ix = np.argwhere(self.final_count[i] == y[i])
+            print("x[i]: {} | y[i]: {}".format(x[i], y[i]))
+            x_ix = np.argwhere(self.unique_attributes[:, i] == x[i]).flatten()
+            y_ix = np.argwhere(self.unique_attributes[:, i] == y[i]).flatten()
+            print("self.final_count: {}".format(self.final_count))
+            print("self.final_count[i]: {}".format(self.final_count[i]) )
+            print("x_ix: {} | y_ix: {}".format(x_ix, y_ix))
             
-            N_ax = self.final_count[i, x_ix, :-1]
-            N_ay = self.final_count[i, y_ix, :-1]
-            N_axc = self.final_count[i, x_ix]
-            N_ayc = self.final_count[i, y_ix]
-
-            temp_result = abs(N_ax/N_axc - N_ay/N_ayc)
-            temp_result = np.sum(temp_result)
+            N_ax = self.final_count[i, x_ix, -1].flatten()
+            N_ay = self.final_count[i, y_ix, -1].flatten()
+            N_axc = self.final_count[i, x_ix].flatten()
+            N_ayc = self.final_count[i, y_ix].flatten()
+            print("N_ax: {} N_ay: {} N_axc: {} N_ayc: {}".format(N_ax, N_ay, N_axc, N_ayc))
+            if N_ax != 0 and N_ay != 0:
+                temp_result = abs(N_axc/N_ax - N_ayc/N_ay)
+                temp_result = np.sum(temp_result)
+            else:
+                ValueError("Division by zero is not allowed!")
+            print("temp_result: {}".format(temp_result))
 
             result[i] = temp_result
-
+        print("final_result: {}".format(result))
         return np.sum(result)
     
--- VDM_test.py
+++ VDM_test.py
@@ -4,8 +4,9 @@ from VDM import VDM
 
 @pytest.fixture
 def params():
-    return {'data': np.asarray([[3, 2, 3], [4, 2, 2], [4, 1, 3], [1, 2, 3], [3, 1, 2]]),
-            'cat_ix': [1, 2], 'y_ix': [0], 'nan_eqvs': [np.nan],
+    return {'data': np.asarray([[3, 2, 3, 1], [4, 2, 2, 2], [4, 1, 3, 1], [1, 2, 3, 2], [3, 1, 2, 1],
+            [1, 2, 2, 1], [1, 1, 2, 1]]),
+            'cat_ix': [1, 2, 3], 'y_ix': [0], 'nan_eqvs': [np.nan],
             'result': [3, 1, 2]}
 
 def test_init(params):
@@ -17,6 +18,7 @@ def test_vdm(params):
     data = params['data']
     results = params['result']
     final_count = metric.final_count
+    result = metric.vdm(data[0], data[1])
     #assert results[0] == round(metric.heom(data[0], data[1]), 2)
     #assert results[1] == round(metric.heom(data[0], data[2]), 2)
     #assert results[2] == round(metric.heom(data[1], data[2]), 2)

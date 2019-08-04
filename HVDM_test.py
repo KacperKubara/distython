b773ee4a725a2bb58c2d2e680812e873497d4828
Kacper Kubara <kjk1u17@soton.ac.uk>
2019-08-04 19:41:01 +0100

    Test update

--- HVDM_test.py
+++ HVDM_test.py
@@ -5,7 +5,7 @@ from HVDM import HVDM
 @pytest.fixture
 def params():
     return {'data': np.asarray([[1, 1, np.nan, 5], [2, 2, 2, 2], [1, 1, 5, 3]]),
-             'cat_ix': [1], 'y_ix': [0],'nan_eqvs': [np.nan], 'result': [6.5, 1.1, 5.4]}
+             'cat_ix': [1], 'y_ix': [0],'nan_eqvs': [np.nan], 'result': [5.64, 1.16, 4.57]}
 
 def test_init(params):
     metric = HVDM(params['data'], params['y_ix'],params['cat_ix'], params['nan_eqvs'])
